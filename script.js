85e4f9a9baf3fab0834291d10eed2489a1d43f11
david <dumedavid11@gmail.com>
2022-06-04 21:20:25 +0200

    added basic calculations

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// basic functions
+const add = (a, b) => Number(a) + Number(b);
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
 const addBtn = document.querySelector('#add');
 const subtractBtn = document.querySelector('#subtract');
 const multiplyBtn = document.querySelector('#multiply');
@@ -6,27 +12,47 @@ const divideBtn = document.querySelector('#divide');
 const displaySpan = document.querySelector('.result_num');
 
 const numArray = document.querySelectorAll('.number');
+const operatorArr = document.querySelectorAll('.operator');
+const resultDisplay = document.querySelector('#result');
 
-let selectedNum = '';
+let isFirstNum = false;
+let isSecondNum = false;
 
-const displayNum = n => {
-    
-    selectedNum += n;
-    displaySpan.textContent =`${selectedNum}`
+let firstNum = '';
+let secondNum = '';
+let operation;
+
+//doesnt work for numbers bigger than 10... check the isFirstNum and isSecondNum values
+const displayNum = n => { 
+    if(!isFirstNum) {
+        firstNum += n;
+        displaySpan.textContent =`${firstNum}`
+        isFirstNum = true;
+    } else if(!isSecondNum) {
+        secondNum += n;
+        displaySpan.textContent =`${secondNum}`
+        isSecondNum = true
+    }  
 }
+
+/* const makeOperation = (op) => {   
+    operation = op
+
+} */
+
+resultDisplay.addEventListener('click', e => {
+    if(operation === 'add') displaySpan.textContent = add(firstNum, secondNum);
+    if(operation === 'subtract') displaySpan.textContent =  subtract(firstNum, secondNum);
+    if(operation === 'multiply') displaySpan.textContent =  multiply(firstNum, secondNum);
+    if(operation === 'divide') displaySpan.textContent =  divide(firstNum, secondNum);
+})
 
 numArray.forEach(n => n.addEventListener('click', e => displayNum(e.target.value)));
+operatorArr.forEach(o => o.addEventListener('click', e => {
+    operation = e.target.value;
+    displaySpan.textContent = ''
+}));
 
 
-// basic functions
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
 
-const operate = (op, a, b) => {
-    if(op === 'add') return add(a, b);
-    if(op === 'subtract') return subtract(a, b);
-    if(op === 'multiply') return multiply(a, b);
-    if(op === 'divide') return divide(a, b);
-}
+
